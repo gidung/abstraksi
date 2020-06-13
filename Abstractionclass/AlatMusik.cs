b2f7533dd467b7ad057226de35d7358f1ff40ec2
gidung <gidung.id@gmail.com>
2020-06-13 23:21:38 +0700

    tugas abstraksi

---
+++ Abstractionclass/AlatMusik.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Abstraction.Abstractclass
+{
+    public abstract class AlatMusik
+    {
+        public abstract void Bunyi();
+    }
+}
---
+++ Abstractionclass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Abstraction.Abstractclass;
+
+namespace Abstractionclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlatMusik alatMusik;
+
+            alatMusik = new Piano();
+            alatMusik.Bunyi();
+
+            Console.ReadKey();
+        }
+    }
+}
